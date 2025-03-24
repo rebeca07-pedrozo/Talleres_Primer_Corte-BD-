d019ae9b78a6a107f2ffc5a217cda1cbf4478267
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-03-24 18:38:42 -0500

    Iniciar sesion y crear cuenta

--- Index.php
+++ Index.php
@@ -5,16 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-          rel="stylesheet" 
-          integrity="sha384-1BmE4KWBq78iYhFldvKuhfTAU6auU8tT9WrHftjDbrCEXSU1oBoqy12QvZ6jIW3" 
-          crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+          integrity="sha384-1BmE4KWBq78iYhFldvKuhfTAU6auU8tT9WrHftjDbrCEXSU1oBoqy12QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Inicio de sesión</title>
-</head>
+    </head>
 <body>
-    
+    <form action=""> 
+        <i class="fa-solid fa-user"></i> 
+        <label>Usuario</label>
+        <input type="text" name="Usuario" placeholder="Nombre de usuario">
+        <i class="fa-solid fa-unlock"></i> 
+        <label>Clave</label>
+        <input type="password" name="Clave" placeholder="Clave">
+        <button type="submit">Iniciar Sesión</button> 
+        <a href="CrearCuenta.php">Crear Cuenta</a> 
+    </form>
 </body>
 </html>
+
